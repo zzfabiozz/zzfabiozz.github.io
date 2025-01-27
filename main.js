4b8e2a01236d08a52ec2d437dc19466cb6aa3d02
f4b1o22 <43100584+zzfabiozz@users.noreply.github.com>
2025-01-27 11:02:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 
 const about = document.querySelector('#about')
 const contact = document.querySelector('#contact')
+const contact = document.querySelector('#contact')
 const writeup = document.querySelector('#writeup')
 const certificates = document.querySelector('#Certificates')
 
 // edit contain 2
 const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
 const contactContent = document.querySelector('#contact-content')
 const writeupContent = document.querySelector('#writeup-content')
 const certificatesContent = document.querySelector('#certificates-content')
@@ -30,6 +32,26 @@ about.addEventListener('click', () => {
     },
   })
 })
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    width: '400px',
+    height: '400px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 60,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
 
 contact.addEventListener('click', () => {
   const contactBox = new WinBox({
